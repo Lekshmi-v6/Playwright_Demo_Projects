095ad1c05f66ad9ac0d81ae2f8236fe9bfd4cf4e
Lekshmi V <lekshmi.vn@pitsolutions.com>
2026-04-04 07:08:39 +0530

    Added KPI automation test

--- pages/Performance.ts
+++ pages/Performance.ts
@@ -9,9 +9,64 @@ private page:Page;
         this.page=page;
     }
 
-get PerformanceReview(){
+async PerformanceReview(){
 
-    return this.page.getByRole('link', { name: 'Performance' });
+    await this.page.getByRole('link', { name: 'Performance' }).click();
+}
+ async Configure(){
+ const configureLink = this.page.getByText('Configure', { exact: true });
+await configureLink.waitFor({ state: 'visible', timeout: 10000 });
+await configureLink.click();
+    
+ }
+async KPLs(){
+
+  const kpi = this.page.getByRole('menuitem', { name: 'KPIs' });
+
+  // Wait until visible
+  await kpi.waitFor({ state: 'visible', timeout: 10000 });
+
+  // Click
+  await kpi.click();
+  //await this.page.locator('.oxd-select-text').click();
+ //const job= await this.page.getByRole('option', { name: 'QA Lead', exact: true }).click();
+}
+async kpiAdd(){
+    await this.page.getByRole('button', {name: ' Add '}).click();
+    await this.page.getByRole('textbox').nth(1).fill('Planning MethodologiesTest');
+    await this.page.locator('.oxd-select-text').click();
+    await this.page.getByRole('option', { name: 'QA Lead'}).click();
+    await this.page.getByRole('textbox').nth(2).fill('10');
+    await this.page.getByRole('textbox').nth(3).fill('80');
+    await this.page.locator('.oxd-switch-input').click();
+    await this.page.getByRole('button' ,{name: 'Save'}).click();
+   
+   
+}
+async KPISearch(){
+    await this.page.getByRole('button', { name: 'Search' }).click();
+    const Recordcount=await this.page.locator('.oxd-table-card').count();
+    for(let i=0;i<Recordcount;i++){
+
+        const KPindicator=this.page.locator('.oxd-table-card').nth(i);
+        const kpcloumn= await KPindicator.locator('.oxd-table-cell').nth(1).textContent();
+        console.log(kpcloumn);
+       if (kpcloumn === 'Planning MethodologiesTest')
+        {
+        await KPindicator.locator('.oxd-icon.bi-pencil-fill').click();
+           break; 
+       }
+  
+    }
+   
+   
+}
+async kpiEdit(){
+const minRate = await this.page.locator('.oxd-grid-4 .oxd-grid-item .oxd-input-group input').first();
+await minRate.click();
+await minRate.fill('5');
+await this.page.locator('.oxd-switch-input').click();
+await this.page.getByRole('button' ,{name: 'Save'}).click();
 
 }
 }
--- tests/ormPerformance.spec.ts
+++ tests/ormPerformance.spec.ts
@@ -1,11 +1,24 @@
 import {test,expect} from '../fixture/Testdataorange';
+import {orangeLogin} from '../pages/orangeLogin';
 import { Performance } from "../pages/Performance";
 let performance :Performance;
 
-test('Perfomance Module',async ({page})=>{
+test('Perfomance Module',async ({page,testData})=>{
+    const orangelogin= new orangeLogin(page);
+    await orangelogin.goto();
+
+    await orangelogin.Login(testData.validUser.username,testData.validUser.password);
+    await expect(page).toHaveURL(/dashboard/);
+
      performance = new Performance(page);
-    await performance.PerformanceReview.click();
+    await performance.PerformanceReview();
+     await expect(page).toHaveURL(/performance/);
+     
+     await performance.Configure();
+     await performance.KPLs();
+     await performance.kpiAdd();
+     await performance.KPISearch();
+     await performance.kpiEdit();
+    await page.pause();
     
-
-
 });
